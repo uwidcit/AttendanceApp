5bfb5ca39df32280ac3bbe1610b3c603e7d12ec4
mdls8 <mdls85>
2016-04-23 23:46:02 -0400

    Added comments to course analytics

--- app/src/main/java/com/jevon/studentrollrecorder/helpers/SortByDateHelper.java
+++ app/src/main/java/com/jevon/studentrollrecorder/helpers/SortByDateHelper.java
@@ -11,6 +11,8 @@ import java.util.Locale;
  * Created by mdls8 on 4/18/2016.
  */
 public class SortByDateHelper implements Comparator<Session> {
+
+    // determines the order in which sessions occurred
     public int compare(Session s1, Session s2){
         long s1Time = convertDate(s1);
         long s2Time = convertDate(s2);
@@ -24,6 +26,7 @@ public class SortByDateHelper implements Comparator<Session> {
         return 0;
     }
 
+    // returns a date stored as a long from the string date field of a session
     private long convertDate(Session session){
         SimpleDateFormat dateFormat = new SimpleDateFormat("EEE dd-MM-yy H", Locale.ENGLISH);
         Date sessionDate = new Date();
